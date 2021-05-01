34bbf409d3a26799b93340d7386953d74b1f61dd
zhouxiaomin <lindazhoucuc@163.com>
2021-05-01 16:40:58 +0800

    36validsudoku set

--- 36_Valid_sudoku.cpp
+++ 36_Valid_sudoku.cpp
@@ -1,12 +1,16 @@
 //
 // Created by zhouxiaomin on 2021/05/01.
 //
+#include <iostream>
 #include <vector>
+#include <string>
+#include <set>
 using namespace std;
 
 //对于当前board[i][j] 检查当前所处行、列、小方块，没有重复的
 class Solution {
 public:
+    //方法1 Runtime 12 ms	Memory18 MB
     bool isValidSudoku(vector<vector<char>>& board) {
         for(int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
@@ -38,6 +42,41 @@ public:
         }
         return true;
     }
+
+    //方法2 Runtime 28 ms	Memory20.1 MB
+    bool isValidSudoku2(vector<vector<char>>& board) {
+        set<string> myset;
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                char ch = board[i][j];
+                if(ch == '.') continue;
+
+                string key = string(1, ch);
+                string inRow = key + "in row" + to_string(i);
+                string inCol = key + "in col" + to_string(j);
+                int rowIndex = i/3;
+                int colIndex = j/3;
+                string inSubBox = key + "in subbox" + to_string(rowIndex) + to_string(colIndex);
+
+                if(myset.find(inRow) != myset.end() || myset.find(inCol) != myset.end() ||
+                        myset.find(inSubBox) != myset.end()) {
+                    return false;
+                } else {
+                    myset.insert(inRow);
+                    myset.insert(inCol);
+                    myset.insert(inSubBox);
+                }
+            }
+        }
+        return true;
+    }
+
 };
 
+
+int main() {
+    char ch = '5';
+    string t = string(3, ch);   //string 构造函数 fill string (size_t n, char c);
+    cout << t << endl;
+}
 
